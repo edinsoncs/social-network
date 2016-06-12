0e0a6b7395c4393442424ce7930cf9fac92b535f
edinson <jean_clude_dortosms@hotmail.com>
2016-06-12 10:00:15 -0300

    fixes

--- public/javascripts/viaintifrontendbackend.js
+++ public/javascripts/viaintifrontendbackend.js
@@ -143,7 +143,7 @@ $(document).ready(function(){
 
         function inCity(lon, lat) {
 
-            $.getJSON('http://maps.googleapis.com/maps/api/geocode/json' + '?latlng=' + lat + ',' + lon, showInCity, false);
+            $.getJSON('https://maps.googleapis.com/maps/api/geocode/json' + '?latlng=' + lat + ',' + lon, showInCity, false);
             
             function showInCity(data) {
 
@@ -241,7 +241,12 @@ $(document).ready(function(){
 
             }
 
-        });
+
+          });
+
+        }
+
+        else if {
 
         }
      
